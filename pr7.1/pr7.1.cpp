cdf7bfd5e747c544338e767ce37e93d77e11114e
AntonBoev14 <antonboev421@gmail.com>
2024-11-07 10:06:09 +0300

    Add files via upload

---
+++ pr7.1/pr7.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+bool isPrime(int number) {
+    if (number <= 1) {
+        return false; 
+    }
+    for (int i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            return false; 
+        }
+    }
+    return true; 
+}
+
+int main() {
+    setlocale(0, "ru");
+    int num;
+
+    do {
+        std::cout << "Введите целое число\n -> ";
+        std::cin >> num; 
+
+        if (isPrime(num)) {
+            std::cout << num << " - простое число." << std::endl;
+        }
+        else {
+            std::cout << num << " - не простое число." << std::endl;
+        }
+
+    } while (false); 
+
+    return 0;
+}
